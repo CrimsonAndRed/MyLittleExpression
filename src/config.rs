ec5b5c70d533e51d20c2ebffe8bbb5435bf8e7b1
Danone <crimsonandred@mail.ru>
2019-11-11 11:51:11 +0300

    Fix operations to nonoverflow

--- src/config.rs
+++ src/config.rs
@@ -18,13 +18,19 @@ impl<T> Default for ParserConfig<T> where T: Operand {
 
 impl ParserConfig<i64> {
     pub fn with_basic_math(&mut self) {
-        let plus = Operator::new('+', true, |a, b| Ok(a + b), 100);
-        let minus = Operator::new('-', true, |a, b| Ok(a - b), 100);
-        let multiply = Operator::new('*', true, |a, b| Ok(a * b), 200);
-        let divide = Operator::new('/', true, |a, b| Ok(a / b), 200);
-        let power = Operator::new('^', false, |a, b| {
-            // TODO https://docs.rs/num-traits/0.2.5/src/num_traits/cast.rs.html#197-204
-            Ok(i64::pow(*a, *b as u32))
+        let plus = Operator::new('+', true, |a: &i64, b| a.checked_add(*b).ok_or_else(|| format!("Add with overflow: {} + {}", a, b)), 100);
+        let minus = Operator::new('-', true, |a: &i64, b| a.checked_sub(*b).ok_or_else(|| format!("Subtract with overflow: {} - {}", a, b)), 100);
+        let multiply = Operator::new('*', true, |a: &i64, b| a.checked_mul(*b).ok_or_else(|| format!("Multiplication with overflow: {} * {}", a, b)), 200);
+        let divide = Operator::new('/', true, |a: &i64, b| a.checked_div(*b).ok_or_else(|| format!("Division with overflow: {} / {}", a, b)), 200);
+        let power = Operator::new('^', false, |a: &i64, b| {
+            if *b < 0 {
+                return Err(format!("Could not calculate negative exponent: {} ^ {}", a, b));
+            }
+
+            if *b > std::u32::MAX as i64 {
+                return Err(format!("Too big exponent: {} ^ {}", a, b));
+            }
+            i64::checked_pow(*a, *b as u32).ok_or_else(|| format!("Exponent with overflow: {} ^ {}", a, b))
         }, 300);
 
         self.operators.insert(plus.symbol, plus);
@@ -43,7 +49,6 @@ impl ParserConfig<f64> {
         let multiply = Operator::new('*', true, |a, b| Ok(a * b), 200);
         let divide = Operator::new('/', true, |a, b| Ok(a / b), 200);
         let power = Operator::new('^', false, |a, b| {
-            // TODO https://docs.rs/num-traits/0.2.5/src/num_traits/cast.rs.html#197-204
             Ok(f64::powf(*a, *b))
         }, 300);
 
